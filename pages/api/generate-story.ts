17e5a8e2e53960a273536054632bc4966bf4e50b
Lars Grammel <lars.grammel@gmail.com>
2023-09-15 11:12:03 +0300

    Extract files.

--- pages/api/generate-story.ts
+++ pages/api/generate-story.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { z } from "zod";
+import { AsyncQueue } from "@/lib/AsyncQueue";
 
 export const config = {
   runtime: "edge",
@@ -61,55 +62,4 @@ function createTextDeltaEventSource(
       enqueueData(controller, { isFinished: true });
     },
   });
-}
-
-/**
- * @internal
- */
-export class AsyncQueue<T> implements AsyncIterable<T | undefined> {
-  queue: T[];
-  resolvers: Array<(options: { value: T | undefined; done: boolean }) => void> =
-    [];
-  closed: boolean;
-
-  constructor() {
-    this.queue = [];
-    this.resolvers = [];
-    this.closed = false;
-  }
-
-  push(value: T) {
-    if (this.closed) {
-      throw new Error("Pushing to a closed queue");
-    }
-
-    const resolve = this.resolvers.shift();
-    if (resolve) {
-      resolve({ value, done: false });
-    } else {
-      this.queue.push(value);
-    }
-  }
-
-  close() {
-    while (this.resolvers.length) {
-      const resolve = this.resolvers.shift();
-      resolve?.({ value: undefined, done: true });
-    }
-    this.closed = true;
-  }
-
-  [Symbol.asyncIterator]() {
-    return {
-      next: (): Promise<IteratorResult<T | undefined, T | undefined>> => {
-        if (this.queue.length > 0) {
-          return Promise.resolve({ value: this.queue.shift(), done: false });
-        } else if (this.closed) {
-          return Promise.resolve({ value: undefined, done: true });
-        } else {
-          return new Promise((resolve) => this.resolvers.push(resolve));
-        }
-      },
-    };
-  }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { AsyncQueue } from "@/lib/AsyncQueue";
 import { parseEventSourceReadableStream } from "@/lib/parseEventSourceReadableStream";
 
 export default function Home() {
@@ -60,56 +61,4 @@ export function readEvents(
   });
 
   return queue;
-}
-
-/**
- * @internal
- */
-// TODO figure out how to remove the undefined
-export class AsyncQueue<T> implements AsyncIterable<T | undefined> {
-  queue: T[];
-  resolvers: Array<(options: { value: T | undefined; done: boolean }) => void> =
-    [];
-  closed: boolean;
-
-  constructor() {
-    this.queue = [];
-    this.resolvers = [];
-    this.closed = false;
-  }
-
-  push(value: T) {
-    if (this.closed) {
-      throw new Error("Pushing to a closed queue");
-    }
-
-    const resolve = this.resolvers.shift();
-    if (resolve) {
-      resolve({ value, done: false });
-    } else {
-      this.queue.push(value);
-    }
-  }
-
-  close() {
-    while (this.resolvers.length) {
-      const resolve = this.resolvers.shift();
-      resolve?.({ value: undefined, done: true });
-    }
-    this.closed = true;
-  }
-
-  [Symbol.asyncIterator]() {
-    return {
-      next: (): Promise<IteratorResult<T | undefined, T | undefined>> => {
-        if (this.queue.length > 0) {
-          return Promise.resolve({ value: this.queue.shift(), done: false });
-        } else if (this.closed) {
-          return Promise.resolve({ value: undefined, done: true });
-        } else {
-          return new Promise((resolve) => this.resolvers.push(resolve));
-        }
-      },
-    };
-  }
 }
