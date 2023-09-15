f7cb05e3d0a004c65e8e6dff38f2b57822102e63
Lars Grammel <lars.grammel@gmail.com>
2023-09-15 12:21:52 +0300

    Add example narration arcs.

---
+++ src/story/generateNarrationArc.examples.ts
@@ -0,0 +1,117 @@
+export const generateNarrationArcExamples = [
+  {
+    title: "The Prince and the Pirate",
+    introduction:
+      "Once upon a time, in a far ocean kingdom's palace, a brave young Prince yearned for adventure. On another part of the sprawling sea, a kind-hearted Pirate sought peace and friendship, tired of loneliness among his rough, ancient crew.",
+    risingAction:
+      "One day, a mysterious message in a bottle floated ashore in the kingdom. The Prince, curiosity sparked, set out on a daring voyage across the treasure-laden isles. At the same time, the Pirate discovered the very same message, living dormant in his trusty old chest.",
+    climax:
+      "As the story unravels, the paths of the Prince and the Pirate inadvertently collide amidst stormy seas and chases, with prayers and hopes entwined. As they come face to face, they not only discover that life contains more riches than gold, but also that courage and friendship shape a secret key unlocking true praise.",
+    fallingAction:
+      "With clashes settled, animosities quelled and someone mysterious to rescue, both forge a bond braving dense jungles, mountains high and valleys deep, their quests humorously tangled yet courageously fought.",
+    conclusion:
+      "In the end, it was friendship's magical strength and unity of their protagonistic spirits that led to their triumphant fulfillment- rescuing not just a selfish king's lost treasure, as told in the captured messages, but also releasing a kingdom trapped under his self-serving reign. The exodus ends with the royals returning to their homes and the pirate now feared less, entrusted with keeping the ocean’s peace. This story signifies that often, our characterized identities can transform when propped by unity and moral purpose.",
+    characters: [
+      {
+        name: "Prince",
+        gender: "Male",
+        ageInYears: 18,
+        voice: "bold and determined",
+      },
+      {
+        name: "Pirate",
+        gender: "Male",
+        ageInYears: 25,
+        voice: "husky yet comforting",
+      },
+    ],
+  },
+  {
+    title: "The Prince and The Pirate",
+    introduction:
+      "In a world divided by land and sea, a brave Prince and a masterly Pirate lived their separate lives. Despite being incredibly different, they share the dream of being free from the stereotypes attached to their identities.",
+    risingAction:
+      "One day, fate merges their paths when both the Prince and the Pirate, on separate voyages; find a treasure map leading to the legendary Lost Diamond of Atlantis. Both start their journey towards the treasure.",
+    climax:
+      "Their paths cross in the middle of the stormy sea, both sailing towards their dream. The differences between them take a backseat, and the brave Prince and the masterly pirate fight a fierce sea monster protecting the lost diamond.",
+    fallingAction:
+      "After defeating the sea monster, they realize each other's capabilities and mutuality formed from shared dreams. They split the treasure and depart towards their separate paths with a newfound understanding and respect for the other's identity.",
+    conclusion:
+      "In spite of being of different worlds - the Sea and the Land, the Prince and the Pirate understood their similarities and pursued their shared dreams, breaking free from the shackles of stereotypes. They learned valuable lessons on the power of unity, the importance of mutual respect, and freedom of choice.",
+    characters: [
+      {
+        name: "Prince Aron",
+        gender: "Male",
+        ageInYears: 20,
+        voice: "Confident and clear",
+      },
+      {
+        name: "Captain Morgan",
+        gender: "Male",
+        ageInYears: 35,
+        voice: "Rough and tough",
+      },
+    ],
+  },
+  {
+    title: "The Adventures of Prince Peter and Pirate Polly",
+    introduction:
+      "Once upon a time, in a kingdom captured by the magic of innocence and childhood bliss, lived an electric spirited Prince, Peter. One fateful day, he crossed paths with Polly, a fiery and confident pirate girl from a distant land.",
+    risingAction:
+      "Surprising Prince Peter, Pirate Polly was far from a villain. She believed in adventure, exploring over unfamiliar waters, seeking really vaguely familiar treasures. They planned a grand quest to seek an obscured key that was said to disclose the path to the mysterious 'laughter island'.",
+    climax:
+      "Their voyage was met with a rumbling storm and gritty challenges. Peter, armed with bravery from his royal blood, navigated them through boiling sea waves. Polly, never confounded by mishaps, turned seemingly unfortunate situations around with her quick-wittedness.",
+    fallingAction:
+      "The wave retreats came with refreshing peace and they discovered the Kindness Key amidst a joyful dolphin colony. As the key was inserted into an age-old stone threshold, they experienced a terrain unfold gleaming bright with incessantly contagious laughter...",
+    conclusion:
+      "The tangling journey while daunting, taught Prince Peter about real adventure, and Pirate Polly about tempered bravery. Their offbeat friendship weaved a humbling interconnectedness as it unveiled a more heartfelt joy beyond laugh-filled treasures.",
+    characters: [
+      {
+        name: "Prince Peter",
+        gender: "male",
+        ageInYears: 6,
+        voice: "Caricature-like, with buoyant hints of responsible calmness.",
+      },
+      {
+        name: "Pirate Polly",
+        gender: "female",
+        ageInYears: 6,
+        voice:
+          "Assertive with a playful undertow and an adventurous eagerness in equal parts.",
+      },
+    ],
+  },
+  {
+    title: "The Prince and the Pirate",
+    introduction:
+      "Once upon a time, in a grand kingdom by the sea, lived a young Prince named Leo. Leo is fascinated by thrilling stories and adventures of pirates he's often heard by the seashore sailors.",
+    risingAction:
+      "One day, a real pirate named Jack gets washed up on the beach near the castle. Jack, hoping to find some treasure to recover on his path, gets intercepted by and befriends the adventurous Prince Leo. Rebellious Jack intrigues Leo with his raucous stories about the seas.",
+    climax:
+      "When a wicked and greedy neighboring king threatens to take to take control of Prince Leo's kingdom, the prince and Jack decides to take matters into their own hands. Planning a cunning play, they decide to pretend to find the 'magic treasure' that would make the king retreat.",
+    fallingAction:
+      "Prince Leo and Jack pull off their smokescreen commendably. It disturbs the greedy king, who then quickly decides to retreat & withdraw out of fear. Their daunting ordeal finally came to an end, and the kingdom harmony is restored.",
+    conclusion:
+      "Behind every daunting pirates tale, was a brave story of friendship and courage. Prince Leo gained a new friend and some spicy sailing stories experienced in real; so did the pirate who adds his presence in the royal prince's thrilling life by pledging loyalty to him.",
+    characters: [
+      {
+        name: "Leo",
+        gender: "male",
+        ageInYears: 10,
+        voice: "happy and enthusiastic",
+      },
+      {
+        name: "Jack",
+        gender: "male",
+        ageInYears: 22,
+        voice: "grunt with an unrefined pirate accent",
+      },
+      {
+        name: "The bitter king",
+        gender: "male",
+        ageInYears: 35,
+        voice: "arrogant and snobby",
+      },
+    ],
+  },
+];
