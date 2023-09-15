fd4c24d794c049402949508be2cab4ce1fb2a6f3
Lars Grammel <lars.grammel@gmail.com>
2023-09-15 10:37:42 +0300

    Add basic event streaming.

--- pages/api/generate-story.ts
+++ pages/api/generate-story.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { z } from "zod";
 
 export const config = {
+  runtime: "edge",
   api: { bodyParser: true },
 };
 
@@ -13,11 +15,101 @@ export default async function handler(
     return;
   }
 
-  try {
-    res.status(200).json({ hello: "world" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: error?.toString(), error });
-    return;
+  // TODO generate an event stream with 3 events
+
+  const queue = new AsyncQueue<string>();
+
+  queue.push("value1");
+  queue.push("value2");
+  queue.close();
+
+  return new Response(createTextDeltaEventSource(queue), {
+    headers: {
+      "Content-Type": "text/event-stream",
+      "Cache-Control": "no-cache",
+      Connection: "keep-alive",
+      "Content-Encoding": "none",
+    },
+  });
+}
+
+const textEncoder = new TextEncoder();
+
+const textDeltaEventDataSchema = z.object({
+  textDelta: z.string().optional(),
+  isFinished: z.boolean(),
+});
+
+type TextDeltaEventData = z.infer<typeof textDeltaEventDataSchema>;
+
+function enqueueData(
+  controller: ReadableStreamDefaultController<unknown>,
+  data: TextDeltaEventData
+) {
+  controller.enqueue(textEncoder.encode(`data: ${JSON.stringify(data)}\n\n`));
+}
+
+function createTextDeltaEventSource(
+  textDeltas: AsyncIterable<string | undefined>
+) {
+  return new ReadableStream({
+    async start(controller) {
+      for await (const textDelta of textDeltas) {
+        enqueueData(controller, { textDelta, isFinished: false });
+      }
+
+      enqueueData(controller, { isFinished: true });
+    },
+  });
+}
+
+/**
+ * @internal
+ */
+export class AsyncQueue<T> implements AsyncIterable<T | undefined> {
+  queue: T[];
+  resolvers: Array<(options: { value: T | undefined; done: boolean }) => void> =
+    [];
+  closed: boolean;
+
+  constructor() {
+    this.queue = [];
+    this.resolvers = [];
+    this.closed = false;
+  }
+
+  push(value: T) {
+    if (this.closed) {
+      throw new Error("Pushing to a closed queue");
+    }
+
+    const resolve = this.resolvers.shift();
+    if (resolve) {
+      resolve({ value, done: false });
+    } else {
+      this.queue.push(value);
+    }
+  }
+
+  close() {
+    while (this.resolvers.length) {
+      const resolve = this.resolvers.shift();
+      resolve?.({ value: undefined, done: true });
+    }
+    this.closed = true;
+  }
+
+  [Symbol.asyncIterator]() {
+    return {
+      next: (): Promise<IteratorResult<T | undefined, T | undefined>> => {
+        if (this.queue.length > 0) {
+          return Promise.resolve({ value: this.queue.shift(), done: false });
+        } else if (this.closed) {
+          return Promise.resolve({ value: undefined, done: true });
+        } else {
+          return new Promise((resolve) => this.resolvers.push(resolve));
+        }
+      },
+    };
   }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { parseEventSourceReadableStream } from "@/lib/parseEventSourceReadableStream";
+
 export default function Home() {
   const handleSend = async () => {
     try {
@@ -7,7 +9,10 @@ export default function Home() {
         body: "",
       });
 
-      console.log(await response.text());
+      const events = readEvents(response.body!);
+      for await (const event of events) {
+        console.log(event);
+      }
     } finally {
     }
   };
@@ -18,4 +23,92 @@ export default function Home() {
       <button onClick={handleSend}>Click me</button>
     </>
   );
+}
+
+export function readEvents(
+  stream: ReadableStream<Uint8Array>,
+  options?: {
+    errorHandler: (error: any) => void;
+  }
+): AsyncIterable<string | undefined> {
+  const queue = new AsyncQueue<string | undefined>();
+
+  // run async (no await on purpose):
+  parseEventSourceReadableStream({
+    stream,
+    callback: {
+      onClose() {
+        queue.close();
+      },
+      onError(error) {
+        options?.errorHandler(error);
+        queue.close();
+      },
+      onEvent(event) {
+        try {
+          // TODO parse with Zod
+          const data = JSON.parse(event.data);
+
+          queue.push(data.textDelta);
+        } catch (error) {
+          options?.errorHandler(error);
+          queue.close();
+        }
+      },
+    },
+  });
+
+  return queue;
+}
+
+/**
+ * @internal
+ */
+// TODO figure out how to remove the undefined
+export class AsyncQueue<T> implements AsyncIterable<T | undefined> {
+  queue: T[];
+  resolvers: Array<(options: { value: T | undefined; done: boolean }) => void> =
+    [];
+  closed: boolean;
+
+  constructor() {
+    this.queue = [];
+    this.resolvers = [];
+    this.closed = false;
+  }
+
+  push(value: T) {
+    if (this.closed) {
+      throw new Error("Pushing to a closed queue");
+    }
+
+    const resolve = this.resolvers.shift();
+    if (resolve) {
+      resolve({ value, done: false });
+    } else {
+      this.queue.push(value);
+    }
+  }
+
+  close() {
+    while (this.resolvers.length) {
+      const resolve = this.resolvers.shift();
+      resolve?.({ value: undefined, done: true });
+    }
+    this.closed = true;
+  }
+
+  [Symbol.asyncIterator]() {
+    return {
+      next: (): Promise<IteratorResult<T | undefined, T | undefined>> => {
+        if (this.queue.length > 0) {
+          return Promise.resolve({ value: this.queue.shift(), done: false });
+        } else if (this.closed) {
+          return Promise.resolve({ value: undefined, done: true });
+        } else {
+          return new Promise((resolve) => this.resolvers.push(resolve));
+        }
+      },
+    };
+  }
 }
