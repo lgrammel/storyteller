863bc3bfcfd0ae82f226f9ea035b55ea5570be2c
Lars Grammel <lars.grammel@gmail.com>
2023-09-27 20:09:33 +0200

    Remove dead code.

--- src/story/selectVoices.examples.ts
+++
@@ -1,9 +0,0 @@
-export const selectVoicesExamples = [
-  {
-    Narrator: "c8ea4f2a-06e6-4d7b-9484-db941bf7c657",
-    Lily: "9db02220-4029-40f1-a807-55d645386d2b",
-    "Sergeant Baize": "abb4aea5-bc72-467d-82bb-c3169a528cde",
-    Bluebeak: "4e95c4a7-95aa-4b1d-bc23-00f7d1d484ea",
-    "Bluebeak's Parents": "maurice",
-  },
-];
