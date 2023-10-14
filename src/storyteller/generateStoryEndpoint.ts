9912928fcb0d3f9bc304dd367bac1a6c6996f282
Lars Grammel <lars.grammel@gmail.com>
2023-10-14 07:00:38 +0200

    Inline generateStoryImage.

--- src/storyteller/generateStoryEndpoint.ts
+++ src/storyteller/generateStoryEndpoint.ts
@@ -7,17 +7,20 @@ import {
   generateAudioStory,
   structuredStorySchema,
 } from "@/storyteller/generateAudioStory";
-import { generateStoryImage } from "@/storyteller/generateStoryImage";
 import { narrateStoryPart } from "@/storyteller/narrateStoryPart";
 import { FullVoiceId, selectVoice } from "@/storyteller/selectVoice";
 import { Voice, voiceSchema } from "@/storyteller/voice";
 import {
   MemoryVectorIndex,
+  OpenAIChatModel,
   OpenAITextGenerationModel,
   OpenAITranscriptionModel,
+  StabilityImageGenerationModel,
   ZodSchema,
+  generateImage,
   generateText,
   getAudioFileExtension,
+  mapInstructionPromptToOpenAIChatFormat,
   transcribe,
 } from "modelfusion";
 import { readFileSync } from "node:fs";
@@ -86,9 +89,38 @@ export const generateStoryEndpoint: Endpoint<
         run.publishEvent({ type: "generated-title", title });
       })(),
 
-      // generate image that represents story:
+      // Generate image that represents story:
       (async () => {
-        const storyImageBase64 = await generateStoryImage(story);
+        const imagePrompt = await generateText(
+          new OpenAIChatModel({
+            model: "gpt-4",
+            temperature: 0,
+            maxCompletionTokens: 500,
+          }).withPromptFormat(mapInstructionPromptToOpenAIChatFormat()),
+          {
+            instruction:
+              "Generate an short image generation prompt (only abstract keywords, max 8 keywords) for the following story:",
+            input: story,
+          },
+          { functionId: "generate-story-image-prompt" }
+        );
+
+        const storyImageBase64 = await generateImage(
+          new StabilityImageGenerationModel({
+            model: "stable-diffusion-xl-1024-v1-0",
+            cfgScale: 7,
+            height: 1024,
+            width: 1024,
+            samples: 1,
+            steps: 30,
+          }),
+          [
+            {
+              text: `${imagePrompt} style of colorful illustration for a preschooler story`,
+            },
+          ],
+          { functionId: "generate-story-image" }
+        );
 
         const imagePath = await run.storeDataAsset({
           name: "story.png",
