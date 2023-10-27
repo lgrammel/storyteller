612111eef0fa75dcff3c5ba4a77d13750c83ce9f
Lars Grammel <lars.grammel@gmail.com>
2023-10-27 11:53:19 +0200

    Add basic error display.

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,9 +29,16 @@ export default function Home() {
   const [activePart, setActivePart] = useState(0);
   const [generatingStory, setGeneratingStory] = useState(false);
   const [shouldAutoPlay, setShouldAutoPlay] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const resetError = () => {
+    setError(null);
+  };
 
   const startRecording = () => {
     if (isRecording) return;
+
+    resetError(); // Clear any previous errors
 
     navigator.mediaDevices
       .getUserMedia({ audio: true })
@@ -50,7 +57,9 @@ export default function Home() {
         setIsRecording(true);
       })
       .catch((error) => {
-        console.error("Error accessing microphone:", error);
+        setError(
+          "Error accessing microphone. Please ensure you have given the necessary permissions."
+        );
       });
   };
 
@@ -113,7 +122,8 @@ export default function Home() {
             },
           });
         } catch (error) {
-          console.error(error);
+          console.error("Error generating story:", error);
+          setError("An error occurred while generating the story:" + error);
         }
       };
 
@@ -135,6 +145,30 @@ export default function Home() {
 
   return (
     <div className="mx-auto p-5 max-w-[542px]">
+      {error && (
+        <div
+          className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 mb-4 rounded relative"
+          role="alert"
+        >
+          <strong className="font-bold">Error </strong>
+          <span className="block sm:inline">{error}</span>
+          <span
+            className="absolute top-0 bottom-0 right-0 px-4 py-3"
+            onClick={resetError}
+          >
+            <svg
+              className="fill-current h-6 w-6 text-red-500"
+              role="button"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <title>Close</title>
+              <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+            </svg>
+          </span>
+        </div>
+      )}
+
       {waitingForUserInput ? (
         <Card>
           <CardHeader>
