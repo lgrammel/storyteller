ffb5e35015eb83d61b8978d90ede37c8ab1b5cad
Lars Grammel <lars.grammel@gmail.com>
2023-10-27 12:10:10 +0200

    Create fastify plugin.

---
+++ src/server/createEndpointPlugin.ts
@@ -0,0 +1,113 @@
+import { FastifyInstance } from "fastify";
+import { withRun } from "modelfusion";
+import { Endpoint } from "./Endpoint";
+import { EndpointRun } from "./EndpointRun";
+import { saveEndpointRunAssets } from "./saveEndpointRunAssets";
+
+export function createEndpointPlugin<INPUT, EVENT>({
+  endpoint,
+}: {
+  endpoint: Endpoint<INPUT, EVENT>;
+}) {
+  return (fastify: FastifyInstance, opts: unknown, done: () => void) => {
+    const runs: Record<string, EndpointRun<EVENT>> = {};
+
+    fastify.post(`/${endpoint.name}`, async (request) => {
+      const run = new EndpointRun<EVENT>({ endpointName: endpoint.name });
+
+      runs[run.runId] = run;
+
+      // body the request body is json, parse and validate it:
+      const input = endpoint.inputSchema.parse(request.body);
+
+      // start longer-running process (no await):
+      withRun(run, async () => {
+        endpoint
+          .processRequest({
+            input,
+            run,
+          })
+          .catch((err) => {
+            console.error(err);
+          })
+          .finally(async () => {
+            run.finish();
+            try {
+              await saveEndpointRunAssets({
+                basePath: "stories",
+                run,
+              });
+            } catch (err) {
+              console.error(err);
+            }
+          });
+      });
+
+      return {
+        id: run.runId,
+        path: `/${endpoint.name}/${run.runId}/events`,
+      };
+    });
+
+    fastify.get(
+      `/${endpoint.name}/:runId/assets/:assetName`,
+      async (request, reply) => {
+        const runId = (request.params as any).runId;
+        const assetName = (request.params as any).assetName;
+
+        const asset = runs[runId]?.assets[assetName];
+
+        const headers = {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Length": asset.data.length,
+          "Content-Type": asset.contentType,
+          "Cache-Control": "no-cache",
+        };
+
+        reply.raw.writeHead(200, headers);
+
+        reply.raw.write(asset.data);
+        reply.raw.end();
+      }
+    );
+
+    fastify.get(`/${endpoint.name}/:id/events`, async (request, reply) => {
+      const runId = (request.params as any).id;
+
+      const eventQueue = runs[runId]?.eventQueue;
+
+      if (!eventQueue) {
+        return {
+          error: `No event queue found for run ID ${runId}`,
+        };
+      }
+
+      const headers = {
+        "Access-Control-Allow-Origin": "*",
+
+        "Content-Type": "text/event-stream",
+        Connection: "keep-alive",
+        "Cache-Control": "no-cache",
+        "Content-Encoding": "none",
+      };
+
+      reply.raw.writeHead(200, headers);
+
+      const textEncoder = new TextEncoder();
+      for await (const event of eventQueue) {
+        if (reply.raw.destroyed) {
+          console.log("client disconnected");
+          break;
+        }
+
+        const text = textEncoder.encode(`data: ${JSON.stringify(event)}\n\n`);
+
+        reply.raw.write(text);
+      }
+
+      reply.raw.end();
+    });
+
+    done();
+  };
+}
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,16 +1,39 @@
+import cors from "@fastify/cors";
+import fastifyStatic from "@fastify/static";
 import dotenv from "dotenv";
+import Fastify from "fastify";
 import { setGlobalFunctionLogging } from "modelfusion";
+import path from "node:path";
 import { generateStoryEndpoint } from "../storyteller/generateStoryEndpoint";
-import { runEndpointServer } from "./runEndpointServer";
+import { createEndpointPlugin } from "./createEndpointPlugin";
 
 dotenv.config();
 
 setGlobalFunctionLogging("basic-text");
 
-runEndpointServer({
-  endpoint: generateStoryEndpoint,
-  port: +(process.env.PORT ?? "3001"),
-}).catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3001;
+const host = process.env.HOST;
+
+export async function main() {
+  try {
+    const server = Fastify();
+
+    await server.register(cors, {});
+    await server.register(fastifyStatic, {
+      root: path.join(__dirname, "..", "..", "out"),
+      prefix: "/",
+    });
+
+    server.register(createEndpointPlugin({ endpoint: generateStoryEndpoint }));
+
+    console.log(`Starting server on port ${port}...`);
+    await server.listen({ port, host });
+    console.log("Server started");
+  } catch (error) {
+    console.error("Failed to start server");
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+main();
